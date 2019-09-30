befd9aac6e538c683652ec0ce67f1eb22148fbb9
W.Bielecki <hardhill@mail.ru>
2019-09-30 09:17:27 +0800

    Update app.js

--- dist/app.js
+++ dist/app.js
@@ -1,3 +1,16 @@
 "use strict";
-var greating = 'Hello Waclaw';
-console.log(greating);
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const body_parser_1 = __importDefault(require("body-parser"));
+const app = express_1.default();
+app.use(body_parser_1.default.json());
+app.use(body_parser_1.default.urlencoded({ extended: true }));
+app.get('/', (req, res, next) => {
+    res.send('REST API server');
+});
+app.listen(8989, () => {
+    console.log('Server runing...');
+});
