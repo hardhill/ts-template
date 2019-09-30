ca673f023c929670c79d45fa01e9b2b929dd78bf
W.Bielecki <hardhill@mail.ru>
2019-09-30 09:12:12 +0800

    updated

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,16 @@
 import express,{Application,Request,Response,NextFunction} from 'express'
+import bodyParser from 'body-parser'
+
 
 const app:Application = express()
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+
 app.get('/',(req:Request,res:Response,next:NextFunction)=>{
-    res.send('Hello')
+    res.send('REST API server')
 })
 
 app.listen(8989,()=>{
